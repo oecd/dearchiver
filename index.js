3331938a30b1a6f23d4b3fe2cc4f63b23cf6bc22
REGNIER Marc <Marc.REGNIER@oecd.org>
2017-03-21 11:17:52 +0100

    clean up

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-var azureStorage = require('azure-storage');
 require('dotenv').config({path: './_env'});
 var express = require('express')
 var fs = require('fs');
 var request = require('request');
-var userhome = require('userhome');
 
-//	The full url to access stored files = 
-//	AZURE_STORAGEACCOUNT + AZURE_CONTAINERNAME
+//	The full url to access stored files
 // 	read _env.sample
 var url = process.env.AZURE_STORAGEACCOUNT + process.env.AZURE_CONTAINERNAME;
 
@@ -22,25 +19,4 @@ app.get('/code/:code', function (req, res) {
 	  	})
 		.pipe(res);	
 })
-
 app.listen(3000)
-
-/*request
-	.get(url + file)
-	.on('error', function(err) {
-	    console.log(err)
-  	})
-	.pipe(fs.createWriteStream(dlDir + file));
-*/
-/*request(url + file, function (error, response, body) {
-	if (error) {
-	  console.log('error:', error); // Print the error if one occurred 
-	} else {
-	  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-	  console.log('dlDir:', dlDir); // Print the HTML for the Google homepage.
-	  fs.writeFile(dlDir + file, body, function (err) {
-		  if (err) throw err;
-		  console.log('It\'s saved!');
-	  })
-	}
-});*/
