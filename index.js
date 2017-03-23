a0d57130d4a8cfe9c505231d6e8e2dfaa6ffabd1
REGNIER Marc <Marc.REGNIER@oecd.org>
2017-03-23 11:58:59 +0100

    add formatting css. TODO: download doesn't work

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config({path: './_env'});
+var bodyParser = require('body-parser');
 var express = require('express')
 var fs = require('fs');
+ var path = require('path')
 var request = require('request');
 
 //	The full url to access stored files
@@ -9,14 +11,45 @@ var url = process.env.AZURE_STORAGEACCOUNT + process.env.AZURE_CONTAINERNAME;
 
 var app = express()
 
-app.get('/code/:code', function (req, res) {
-	var fileName = req.params.code + '.7z';
-	res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
-	request
-		.get(url + fileName)
-		.on('error', function(err) {
-		    console.log(err)
-	  	})
-		.pipe(res);	
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+	res.render('index', {
+		title: 'PAC acrhives', 
+		message: 'Enter your code',
+		formAction: '/code',
+		formMethod: 'post',
+		inputTextAreaType: 'text',
+		inputTextAreaName: 'blobFile',
+		inputButtonType: 'submit',
+		inputButtonValue: 'Dowload'
+	});
 })
+
+app.post('/code', function (req, res) {
+	var fileName = req.body.blobFile + '.7z';
+	try {
+		request(url + fileName, function(err, response) {
+			if (response.statusCode !== 200) {
+				console.log('bad beat!')
+				res.send(
+				'<div>The code ' + req.body.blobFile + ' does not exist</div>' +
+				'<input type="button" value="Back" onclick="window.history.back()"/>'
+				);
+				return
+			}
+			res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
+			res.send(response.body);
+			res.end();
+		})
+		
+	} catch(e) {
+		res.send('File does not exist').end();
+	}
+})
+
 app.listen(3000)
