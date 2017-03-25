c4c9d2dc0d6d0c654d655b6b6331467f16efccae
Jakob Fix <jakob@jfix.com>
2017-03-25 18:53:14 +0100

    Fixed syntax to be js-standard compliant

--- index.js
+++ index.js
@@ -1,55 +1,49 @@
-require('dotenv').config({path: './_env'});
-var bodyParser = require('body-parser');
+require('dotenv').config({path: './_env'})
+var bodyParser = require('body-parser')
 var express = require('express')
 var path = require('path')
-var request = require('request');
+var request = require('request')
 
-//	The full url to access stored files
-// 	read _env.sample
-var url = process.env.AZURE_STORAGEACCOUNT + process.env.AZURE_CONTAINERNAME;
+var url = process.env.AZURE_STORAGEACCOUNT + process.env.AZURE_CONTAINERNAME
 
 var app = express()
-
-app.set('view engine', 'pug');
-
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug')
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist')))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
-	res.render('index', {
-		title: 'PAC acrhives',
-		message: 'Enter your code',
-		inputFileName: 'blobFile',
-	});
+  res.render('index', {
+    title: 'PAC acrhives',
+    message: 'Enter your code',
+    inputFileName: 'blobFile'
+  })
 })
 
 app.post('/code', function (req, res) {
-	var fileName = req.body.blobFile + '.7z';
-    var requestSettings = {
-            method: 'GET',
-            url: url + fileName,
-            encoding: null
-        };
-	try {
-		request(requestSettings, function(err, response) {
-			if (response.statusCode !== 200) {
-				console.log('bad beat!')
-				res.send(
-				'<div>The code ' + req.body.blobFile + ' does not exist</div>' +
-				'<input type="button" value="Back" onclick="window.history.back()"/>'
-				);
-				return
-			}
-			res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
-			res.send(response.body);
-			res.end();
-		})
-
-	} catch(e) {
-		res.send('File does not exist').end();
-	}
+  var fileName = req.body.blobFile + '.7z'
+  var requestSettings = {
+    method: 'GET',
+    url: url + fileName,
+    encoding: null
+  }
+  try {
+    request(requestSettings, function (err, response) {
+      if (err) return console.log(err)
+      if (response.statusCode !== 200) {
+        console.log('bad beat!')
+        res.send(
+          '<div>The code ' + req.body.blobFile + ' does not exist</div>' +
+          '<input type="button" value="Back" onclick="window.history.back()"/>'
+        )
+        return
+      }
+      res.setHeader('Content-disposition', 'attachment; filename=' + fileName)
+      res.send(response.body)
+      res.end()
+    })
+  } catch (e) {
+    res.send('File does not exist').end()
+  }
 })
 
 app.listen(3000)
