bb7df23e8c493f5bad1611ba74b171b541e645b1
REGNIER Marc <Marc.REGNIER@oecd.org>
2017-04-04 13:28:32 +0200

    fix the problem with the control of the ip + add error page

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const bodyParser = require('body-parser')
 require('dotenv').config({path: './_env'})
 const express = require('express')
+const IpDeniedError = require('express-ipfilter').IpDeniedError;
 const ipfilter = require('express-ipfilter').IpFilter;
 const path = require('path')
 const request = require('request')
@@ -14,7 +15,7 @@ app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist')))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-// app.use(ipfilter(['::1', '127.0.0.1', process.env.IP_WHITELIST], {mode: 'allow'}));
+app.use(ipfilter(['::1', '127.0.0.1', process.env.IP_WHITELIST], {mode: 'allow'}));
 
 app.get('/', function (req, res) {
   res.render('index', {
@@ -57,6 +58,19 @@ app.post('/', function (req, res) {
     console.log(e)
   }
 })
+
+app.use(function(err, req, res, _next) {
+  console.log('Error handler', err);
+  if(err instanceof IpDeniedError){
+    res.status(401);
+  }else{
+    res.status(err.status || 500);
+  }
+  res.render('error', {
+    title: 'IP not allowed',
+    error: err
+  });
+});
 
 app.listen(port, function () {
   console.log(`Dearchiver is now listening on port ${port}!`)
