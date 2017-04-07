422cd1efcbd88a016b93ff229df99b304160606e
buzzatwork <marc.regnier@oecd.org>
2017-04-07 12:12:41 +0200

    fixing the problem with ip (Ihope)

--- server.js
+++ server.js
@@ -15,12 +15,37 @@ app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist')))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-//app.use(ipfilter(['::1', '127.0.0.1', process.env.IP_WHITELIST], {
-//  log: true,
-//  logLevel: 'all',
-//  mode: 'allow',
-//  allowedHeaders: ['x-forwarded-for']
-//}));
+app.use(ipfilter(['::1', '127.0.0.1', process.env.IP_WHITELIST], {
+  log: true,
+  logLevel: 'all',
+  mode: 'allow',
+  allowedHeaders: ['x-forwarded-for']
+}));
+
+app.use(function(err, req, res, _next) {
+  if (err instanceof IpDeniedError) {
+    const rejectIp = '78.41.128.14:47172'
+//    const rejectIp = req.header('x-forwarded-for') || req.ip
+    console.log('[error mode: ip = ' + rejectIp + ']')
+      if (rejectIp.substr(0, 7) == '::ffff:') {
+        const trimIp = rejectIp.substr(7)
+        console.log('[trim ip = ' + trimIp + ']')
+        res.render('error', {
+          title: 'Access denied',
+          showRejectedIp: trimIp
+        })
+      }else if (rejectIp !== undefined) {
+        const trimIp = rejectIp.split(':')[0]
+        console.log('[trim ip = ' + trimIp + ']')
+         res.render('error', {
+           title: 'Access denied',
+           showRejectedIp: trimIp
+         })
+      }
+  }else {
+    console.log('[I dont know what am I suppose to do here!]')
+  }
+})
 
 app.get('/', function (req, res) {
   res.render('index', {
@@ -62,37 +87,6 @@ app.post('/', function (req, res) {
     console.log(e)
   }
 })
-
-//app.use(function(err, req, res, _next) {
-  //const getIp = '78.41.128.14:47172'
-//  const getIp = req.header('x-forwarded-for')
-//  const resError = {
-//    title: 'Access denied',
-//    error: err
-//    }
-
-//  try {
-//    if(err instanceof IpDeniedError){
-//      res.status(401)
-//    }else{
-//      res.status(err.status || 500)
-//    }
-
-//    if (getIp !== undefined) {
-//      const splitIp = getIp.split(':' ,1)
-//      console.log('splitted ip = ' + splitIp)
-//      resError.showClientIP = splitIp || req.connection.remoteAddress
-//    }else{
-//      console.log('ip = ' + getIp)
-//      resError.showClientIP = getIp || req.connection.remoteAddress
-//    }
-//    res.render('error', resError)
-//  } catch (e) {
-//    console.log(e)
-//  } finally {
-//    _next()
-//  }
-//})
 
 app.listen(port, function () {
   console.log(`Dearchiver is now listening on port ${port}!`)
